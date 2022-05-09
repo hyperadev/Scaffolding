c1c60287cdcefd7ec358bf13c63e14fd7bc83ed2
= <damian@dmnkchr.com>
2022-05-09 14:41:21 -0500

    feat: make Schematic implement Block.Setter

--- src/main/java/net/crystalgames/scaffolding/schematic/Schematic.java
+++ src/main/java/net/crystalgames/scaffolding/schematic/Schematic.java
@@ -16,7 +16,7 @@ import java.io.OutputStream;
 import java.util.concurrent.CompletableFuture;
 
 @SuppressWarnings("unused")
-public abstract class Schematic {
+public abstract class Schematic implements Block.Setter{
 
     private short[] blocks;
 
